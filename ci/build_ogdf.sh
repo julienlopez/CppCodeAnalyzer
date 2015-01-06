5af1fda3421fa0a0e2255a3f89fd0464311abf78
julienlopez <julien.lopez51@gmail.com>
2015-01-06 16:04:58 +0100

    adding travic-ci build system

--- ci/build_ogdf.sh
+++ ci/build_ogdf.sh
@@ -4,4 +4,8 @@ unzip tech\:ogdf.v2012.07.zip
 cd OGDF
 ./makeMakefile.sh 
 make
+sudo ln -s /home/travis/build/julienlopez/CppCodeAnalyzer/OGDF/ogdf /usr/include
+sudo ln -s /home/travis/build/julienlopez/CppCodeAnalyzer/OGDF/_release/libOGDF.a /usr/lib64
 ls -la
+ls -l /usr/include/ogdf
+ls -l /usr/lib64/libOGDF*
